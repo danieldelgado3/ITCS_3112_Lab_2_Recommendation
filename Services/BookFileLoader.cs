a1670e7b5837817a9892977b7ab78a992bfdf899
daniel <ddelga12@charlotte.edu>
2026-03-19 22:55:33 -0400

    added methods to IBookRepository

--- Services/BookFileLoader.cs
+++ Services/BookFileLoader.cs
@@ -1,6 +1,11 @@
+using ITCS_3112_Lab_2_Recommendation.Contracts;
+
 namespace ITCS_3112_Lab_2_Recommendation.Services;
 
-public class BookFileLoader
+public class BookFileLoader : IFileLoader
 {
-    
+    public void Load(string path)
+    {
+        throw new NotImplementedException();
+    }
 }
