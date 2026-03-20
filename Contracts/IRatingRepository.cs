b1e00283218e7efa7cd26d548a828c5e74c7132b
daniel <ddelga12@charlotte.edu>
2026-03-19 22:57:37 -0400

    added rating class and methods to iratingrepo

--- Contracts/IRatingRepository.cs
+++ Contracts/IRatingRepository.cs
@@ -1,6 +1,12 @@
+using ITCS_3112_Lab_2_Recommendation.Domain;
+using ITCS_3112_Lab_2_Recommendation.Enum;
+
 namespace ITCS_3112_Lab_2_Recommendation.Contracts;
 
 public interface IRatingRepository
 {
-    
+    void SetRating(string memberId, string isbn, RatingValue value);
+    Rating GetRating(string memberId, string isbn);
+    IReadOnlyList<Rating> GetRatingsForMember(string memberId);
+    IReadOnlyList<Rating> GetAll();
 }
