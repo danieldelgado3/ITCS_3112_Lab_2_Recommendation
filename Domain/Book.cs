68057b49a87f29e7e3b4b5ae770e108ec81546c6
daniel <ddelga12@charlotte.edu>
2026-03-19 23:43:45 -0400

    added book and implemented book repo

--- Domain/Book.cs
+++ Domain/Book.cs
@@ -2,5 +2,16 @@ namespace ITCS_3112_Lab_2_Recommendation.Domain;
 
 public class Book
 {
-    
+    public string ISBN { get; }
+    public string Author { get; }
+    public string Title { get; }
+    public int Year { get; }
+
+    public Book(string isbn, string author, string title, int year)
+    {
+        ISBN = isbn;
+        Author = author;
+        Title = title;
+        Year = year;
+    }
 }
--- Repositories/BookRepository.cs
+++ Repositories/BookRepository.cs
@@ -1,6 +1,38 @@
+using ITCS_3112_Lab_2_Recommendation.Contracts;
+using ITCS_3112_Lab_2_Recommendation.Domain;
+
 namespace ITCS_3112_Lab_2_Recommendation.Repositories;
 
-public class BookRepository
+public class BookRepository : IBookRepository
 {
-    
+    private List<Book> _books = new List<Book>();
+
+    public void AddBook(Book book)
+    {
+        if (book is null)
+            throw new ArgumentNullException(nameof(book));
+
+        if (_books.Any(b => b.ISBN == book.ISBN))
+            throw new InvalidOperationException($"Book with ISBN {book.ISBN} already exists");
+
+        _books.Add(book);
+    }
+
+    public Book GetBook(string isbn)
+    {
+        if (string.IsNullOrWhiteSpace(isbn))
+            throw new ArgumentException("ISBN cannot be empty", nameof(isbn));
+
+        Book book = _books.FirstOrDefault(b => b.ISBN == isbn);
+
+        if (book is null)
+            throw new KeyNotFoundException($"Book with ISBN {isbn} not found");
+
+        return book;
+    }
+
+    public List<Book> GetAllBooks()
+    {
+        return _books;
+    }
 }
