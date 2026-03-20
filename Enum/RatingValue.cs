d4b0afd7cdf5e5da42d170b2b40a7c56a7829a89
daniel <ddelga12@charlotte.edu>
2026-03-19 22:51:10 -0400

    implemented enum

--- Enum/RatingValue.cs
+++ Enum/RatingValue.cs
@@ -2,5 +2,10 @@ namespace ITCS_3112_Lab_2_Recommendation.Enum;
 
 public enum RatingValue
 {
-    
+    Hated = -5,
+    Disliked = -3,
+    NotRead = 0,
+    Neutral = 1,
+    Liked = 3,
+    Loved = 5
 }
