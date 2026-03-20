db52e817e9369a41f1e5458727d571f38dfbf8ef
daniel <ddelga12@charlotte.edu>
2026-03-19 23:02:15 -0400

    added methods to iratingservice

--- Contracts/IRatingService.cs
+++ Contracts/IRatingService.cs
@@ -1,1 +1,10 @@
+using ITCS_3112_Lab_2_Recommendation.Domain;
+using ITCS_3112_Lab_2_Recommendation.Enum;
 
+namespace ITCS_3112_Lab_2_Recommendation.Contracts;
+
+public interface IRatingService
+{
+    void RateBook(string memberId, string isbn, RatingValue value);
+    IReadOnlyList<Rating> GetUserRatings(string memberId);
+}
